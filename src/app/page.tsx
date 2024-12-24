0c593745575899fde9357f5cf9c809b0ed66f096
Alstudd <alstonsoares17@gmail.com>
2024-12-24 18:00:59 +0530

    dark mode light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default async function Home() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-5">
-      <div className="flex items-center gap-10">
-        <Bot width={100} height={100} />
-        <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <div className="flex items-center gap-6">
+        <Bot className="lg:w-24 lg:h-24 w-16 h-16" />
+        <span className="text-4xl font-extrabold tracking-tight lg:text-5xl mt-2">
           Solanisation
         </span>
       </div>
-      <p className="max-w-prose text-center">
+      <p className="max-w-prose text-center px-8">
         Welcome to Solanisation, an advanced chatbot that can help you with anything Solana. Click the button below to start chatting.
       </p>
       <Link href="/sign-in">
